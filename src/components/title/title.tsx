357bd2320476aaad7b963b4f251ae6c8de5bc3a6
Valentyn13 <valentyn13git@gmail.com>
2024-07-25 21:06:31 +0300

    fix: fonts and styles

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
+import clsx from 'clsx';
 
 import styles from './title.module.scss';
 
 interface TitleProps {
     title: string;
+    className?: string;
 }
-export const Title: FC<TitleProps> = ({ title }) => {
-    return <h1 className={styles.title}>{title}</h1>;
+export const Title: FC<TitleProps> = ({ title, className = '' }) => {
+    return <h1 className={clsx(styles.title, className)}>{title}</h1>;
 };
